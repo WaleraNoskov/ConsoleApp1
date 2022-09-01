b7453b101bcac372b793c8ff44f39c02e9290fea
Walery <rahdah06@gmail.com>
2022-09-01 20:01:54 +0300

    Task: https://tproger.ru/problems/algoritmicheskaja-zadacha-pro-ostrova/

---
+++ ConsoleApp1/Cartographer.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp1;
+
+/* Coordinates must be in 'y,x' format for arrays!!
+ * 
+ */
+
+public class Cartographer
+{
+    private int[,] map;
+    private int[,] mapChecked;
+    private int mapSizeX;
+    private int mapSizeY;
+    private int IslandCount = 0;
+    
+    public Cartographer(int[,] map)
+    {
+        this.map = map;
+        mapSizeX = map.GetLength(1);
+        mapSizeY = map.Length / mapSizeX;
+        mapChecked = new int[mapSizeX, mapSizeY];
+    }
+
+    public int MakeMap()
+    {
+        for (int y = 0; y < mapSizeY; y++)
+        {
+            for (int x = 0; x < mapSizeX; x++)
+            {
+                if (!IsSectorChecked(x, y))
+                {
+                    if (map[y, x] == 1)
+                    {
+                        IslandCount++;
+                        ResearchArea(x,y);
+                    }
+                    CheckSector(x,y);
+                }
+                
+            }
+        }
+        return this.IslandCount;
+    }
+
+    private void ResearchArea(int x, int y)
+    {
+        CheckSector(x, y);
+        
+        //up
+        if (y > 0 && !IsSectorChecked(x,y-1) && map[y-1,x]==1 )
+            ResearchArea(x,y-1);
+        //left
+        if (x > 0 && !IsSectorChecked(x-1,y) && map[y,x-1]==1 )
+            ResearchArea(x-1,y);
+        //down
+        if (y < mapSizeY-1 && !IsSectorChecked(x,y+1) && map[y+1,x]==1 )
+            ResearchArea(x,y+1);
+        //right
+        if (x < mapSizeX-1 && !IsSectorChecked(x+1,y) && map[y,x+1]==1 )
+            ResearchArea(x+1,y);
+    }
+    
+    private bool IsSectorChecked(int x, int y)
+    {
+        return mapChecked[y, x] == 1;
+    }
+
+    private void CheckSector(int x, int y)
+    {
+        mapChecked[y, x] = 1;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+
+int[,] map =
+{
+    {0,0,0,1,1,0,0},
+    {0,0,0,1,0,0,0},
+    {1,1,0,0,0,0,0},
+    {0,1,0,0,1,0,1},
+    {0,0,0,1,1,1,1},
+    {1,0,0,0,1,0,1},
+    {1,1,0,0,1,0,1},
+};
+
+var mapSizeX = map.GetLength(1);
+var mapSizeY = map.Length / mapSizeX;
+var mapChecked = new int[mapSizeX, mapSizeY];
+
+var cartographer = new Cartographer(map);
+
+Console.WriteLine(cartographer.MakeMap());
